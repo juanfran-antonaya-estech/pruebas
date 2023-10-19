ac6ab3b75f02a727f6d00ea9f318207cd82f1e07
Juan Francisco Antonaya Lopez <juanfran.antonaya@estech.local>
2023-10-19 12:07:52 +0200

    He añadido una funcionalidad para comprobar cuantos intentos toman para encontrar un numero de cifras determinado por el usuario

--- src/CalcularDNI.java
+++ src/CalcularDNI.java
@@ -16,5 +16,24 @@ public class CalcularDNI {
             System.out.println(dni.complete);
         }
 
+        System.out.println("Quieres intentar sacar x cifras de un DNI? 1 si, 2 no");
+        boolean decision = t.nextInt() == 1;
+
+        if (decision) {
+            System.out.println("Escribe la cantidad de cifras que quieres encontrar:");
+            x = t.nextInt();
+            x = (int) Math.pow(10, x);
+            int i = 0;
+            while(true){
+                DNI random = new DNI(DNI.generarnumero());
+                if (random.numero < x) {
+                    System.out.println("Intentos: " + i);
+                    System.out.println("DNI: " + random.complete);
+                    break;
+                }
+                i++;
+            }
+
+        }
     }
 }
